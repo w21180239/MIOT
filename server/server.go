9f645d2a38bee723b5d3867dad94402640ef3ec5
wei <w21180239@163.com>
2019-08-13 19:49:37 +0800

    remove header output

--- server/server.go
+++ server/server.go
@@ -6,17 +6,15 @@ import (
 	"gitlab.com/LICOTEK/DuerOS/duerosprotocol"
 )
 
-import "fmt"
-
 func handlerProtocol() http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Header：")
-		if len(r.Header) > 0 {
-			for k,v := range r.Header {
-				fmt.Printf("%s=%s\n", k, v[0])
-			}
-		}
+		//fmt.Println("Header：")
+		//if len(r.Header) > 0 {
+		//	for k,v := range r.Header {
+		//		fmt.Printf("%s=%s\n", k, v[0])
+		//	}
+		//}
 
 		//fmt.Println("Body：")
 		//buf := new(bytes.Buffer)
